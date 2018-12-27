529dcc2a81265d9f465d10e033c121386650c74c
vpday <i@develop.cool>
2018-12-27 23:35:40 +0800

    :green_heart: remove useless test code.

--- src/test/java/cool/develop/bingwallpaper/test/BasisTest.java
+++ src/test/java/cool/develop/bingwallpaper/test/BasisTest.java
@@ -1,9 +1,5 @@
 package cool.develop.bingwallpaper.test;
 
-import com.blade.kit.JsonKit;
-import cool.develop.bingwallpaper.model.dto.CoverStory;
-import cool.develop.bingwallpaper.model.dto.ImageArchive;
-import io.github.biezhi.request.Request;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -13,38 +9,6 @@ import org.junit.Test;
  */
 @Slf4j
 public class BasisTest extends BaseTest {
-
-    @Test
-    public void requestByCoverStoryTest() {
-        String url = "https://cn.bing.com/cnhp/coverstory";
-
-        Request request = Request.get(url)
-                .acceptJson()
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.62 Safari/537.36");
-        if (!request.ok()) {
-            log.error("请求失败，状态码：{}，错误信息：{}", request.code(), request.message());
-        }
-        String json = request.body();
-
-        CoverStory var = JsonKit.formJson(json, CoverStory.class);
-        System.out.println(var.toString());
-    }
-
-    @Test
-    public void requestByImageArchiveTest() {
-        String url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1";
-
-        Request request = Request.get(url)
-                .acceptJson()
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.62 Safari/537.36");
-        if (!request.ok()) {
-            log.error("请求失败，状态码：{}，错误信息：{}", request.code(), request.message());
-        }
-        String json = request.body();
-
-        ImageArchive imageArchive = JsonKit.formJson(json, ImageArchive.class);
-        System.out.println(imageArchive.toString());
-    }
 
     @Test
     public void parseUrlBaseTest() {
